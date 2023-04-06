17f5c7cf1e8cd8dfaf88af53ed7606feed8e0ae4
Yamba Ouandaogo <yamba.ouandaogo@outlook.com>
2023-04-06 14:43:27 -0500

    Adding exercise HelloWorld

---
+++ src/HelloWorld.java
@@ -0,0 +1,59 @@
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("Hello world");
+        // Favorite number
+        int myFavoriteNumber = 10;
+        System.out.println(" My favorite number is : " +  myFavoriteNumber);
+        // String name
+        String myString = "Henry";
+        System.out.println(myString);
+        //Change to character
+        myString = "H";// it won't display because a character is a different data type
+//        myString = 3.14159; // not the same data type
+        // declare myNumber
+        long myNumber;
+//        System.out.println(myNumber);// variable not initialize
+//        myNumber = 3.14;// requiring double data type;
+        myNumber = 123L;
+        System.out.println(myNumber);
+        myNumber = 123;
+        System.out.println(myNumber);//
+//
+//        float myNumber = 3.14;// myNumber is already defined;
+
+        //increment starting at x .
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String Class = "Henry"; // Class should never be used
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;// a  throw exeption string cannot be cast to number.
+
+//        int three = (int) "three"; // Sting cannot be converted to number
+
+//        int x = 4;
+//          x +=  5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+//        int x = 10;
+//        int y = 2;
+//        x  /= y;
+//        y  -= x;
+//        System.out.println(y);
+
+
+    }
+}
---
+++ src/SyntaxLecture.java
@@ -0,0 +1,9 @@
+public class SyntaxLecture {
+    public static void main(String[] args) {
+        //Declaring a variables
+        int weight = 12;
+        String breed = "Toy Spaniel";
+        String name = "Grinch";
+        System.out.println(name + " is a " + breed);
+    }
+}
